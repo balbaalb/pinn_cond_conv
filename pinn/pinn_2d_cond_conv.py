8c933939a3c9d10fce1fba9af08b578fcc22b323
Bashar Albaalbaki <bashar.albaalbaki@gmail.com>
2024-10-13 08:16:50 -0700

    r eformatting pinn_2d_cond_conv

--- pinn/pinn_2d_cond_conv.py
+++ pinn/pinn_2d_cond_conv.py
@@ -62,6 +62,71 @@ class Txy2:
         return self.alpha + self.beta * self.box.exp(z - self.z_max)
 
 
+class PinnCondConv2D:
+    """
+    Creates a pinn model for the equation
+
+    u ⋅ ∇φ = k Δφ,
+
+    on a 2D rectangular domain. Here Δ is the Laplacian operator, k is diffusivity,
+    and φ is the conserved quantity, ∇ is the graident operator and u is the 2D velocity field.
+    """
+
+    def __init__(
+        self, depths, Lx, Ly, Nx_train, Ny_train, phi_theory_np, kappa, ux, uy
+    ) -> None:
+        self.net = SeqModel(in_features=2, depths=depths, out_features=1)
+        self.X, self.Y, self.XY, self.XY_boundary, self.phi_boundary = (
+            gen_square_domain(
+                Nx=Nx_train, Ny=Ny_train, Lx=Lx, Ly=Ly, boundary_value=phi_theory_np
+            )
+        )
+        self.kappa = kappa
+        self.ux = ux
+        self.uy = uy
+        self.losses = []
+        self.epoch = 0
+
+    def pde_residual(self, phi):
+        phi_x = torch.autograd.grad(
+            phi, self.X, torch.ones_like(phi), create_graph=True, retain_graph=True
+        )[0]
+        phi_xx = torch.autograd.grad(
+            phi_x, self.X, torch.ones_like(phi_x), create_graph=True, retain_graph=True
+        )[0]
+        phi_y = torch.autograd.grad(
+            phi, self.Y, torch.ones_like(phi), create_graph=True, retain_graph=True
+        )[0]
+        phi_yy = torch.autograd.grad(
+            phi_y, self.Y, torch.ones_like(phi_y), create_graph=True, retain_graph=True
+        )[0]
+        residual = self.kappa * (phi_xx + phi_yy) - (self.ux * phi_x + self.uy * phi_y)
+        return residual
+
+    def loss_func(self):
+        self.epoch += 1
+        phi = self.net(self.XY)
+        residual = self.pde_residual(phi=phi)
+        loss_pde = self.criterion(residual, torch.zeros_like(residual))
+        phi_pred_boundary = self.net(self.XY_boundary)
+        loss_bc = self.criterion(phi_pred_boundary, self.phi_boundary)
+        loss = loss_pde + loss_bc
+        self.losses.append(loss.item())
+        self.optimizer.zero_grad()
+        loss.backward()
+        if (self.epoch) % 100 == 0:
+            print(
+                f"Epoch = {self.epoch}, loss = {loss.item()} = {loss_pde.item()} + {loss_bc.item()}"
+            )
+        return loss
+
+    def train(self, lr, epochs):
+        self.criterion = nn.MSELoss()
+        self.optimizer = torch.optim.Adam(params=self.net.parameters(), lr=lr)
+        for _ in range(epochs):
+            self.optimizer.step(self.loss_func)
+
+
 def pinn_2d_cond_conv() -> None:
     """
     Using PINN to solve the conduction-convection conservation equation,
@@ -84,6 +149,9 @@ def pinn_2d_cond_conv() -> None:
     kappa = 1.0
     u = 2.0
     theta_deg = 30.0
+    theta = theta_deg / 180 * np.pi
+    ux = u * np.cos(theta)
+    uy = u * np.sin(theta)
     phi_theory_np = Txy2(
         kappa=kappa,
         phi0=phi0,
@@ -94,104 +162,78 @@ def pinn_2d_cond_conv() -> None:
         y_max=Ly,
         numpy_based=True,
     )
-    title = "ux φ,x + uy φ,y = φ,xx + φ,yy, φ_theory = alpha + beta * exp(z - z_max)"
-    title += "z = (ux * x + uy * y) / kappa"
-    title += "φ(0,0) = φ, φ(x_max, y_max) = φ1"
+    title = "ux φ,x + uy φ,y = φ,xx + φ,yy, φ_theory = alpha + beta * exp(z - z_max), "
+    title += "z = (ux * x + uy * y) / kappa, "
+    title += "φ(0,0) = φ, φ(x_max, y_max) = φ1, "
     title += f"\nφ0 = {phi0}, φ1 = {phi1}, u = {u}, theta_deg = {theta_deg}, kappa = {kappa}, Lx = {Lx}, Ly = {Ly}"
     # ===Parameters================================
     epochs = 10000
     lr = 0.0001
     depths = [64, 64]
-    act_func_type = ACT_FUNCT_TYPE.TANH
     Nx_train = 101
     Ny_train = 101
-    title += f"\nepochs = {epochs}, lr = {lr}, depths = {depths}, N_train = {(Nx_train , Ny_train)}, sigma = {act_func_type}"
+    title += f"\nepochs = {epochs}, lr = {lr}, depths = {depths}, N_train = {(Nx_train , Ny_train)}"
     # ====Training==================================
     torch.manual_seed(731)
-    model = SeqModel(
-        in_features=2, depths=depths, out_features=1, act_func_type=act_func_type
+    model = PinnCondConv2D(
+        depths=depths,
+        Lx=Lx,
+        Ly=Ly,
+        Nx_train=Nx_train,
+        Ny_train=Ny_train,
+        phi_theory_np=phi_theory_np,
+        kappa=kappa,
+        ux=ux,
+        uy=uy,
     )
     file_name = "model_weights_cond_conv.pth"
     model_file = THIS_PATH / file_name
-    X, Y, XY, XY_boundary, phi_boundary = gen_square_domain(
-        Nx=Nx_train, Ny=Ny_train, Lx=Lx, Ly=Ly, boundary_value=phi_theory_np
-    )
     duration_mins = 0
     losses = []
     if model_file.exists():
-        model.load_state_dict(torch.load(model_file))
+        model.net.load_state_dict(torch.load(model_file))
     else:
-        criterion = nn.MSELoss()
-        optimizer = torch.optim.Adam(params=model.parameters(), lr=lr)
-        theta = theta_deg / 180 * np.pi
-        ux = u * np.cos(theta)
-        uy = u * np.sin(theta)
+
         t_start = time.time()
-        for i in range(epochs):
-            phi = model(XY)
-            phi_x = torch.autograd.grad(
-                phi, X, torch.ones_like(phi), create_graph=True, retain_graph=True
-            )[0]
-            phi_xx = torch.autograd.grad(
-                phi_x, X, torch.ones_like(phi_x), create_graph=True, retain_graph=True
-            )[0]
-            phi_y = torch.autograd.grad(
-                phi, Y, torch.ones_like(phi), create_graph=True, retain_graph=True
-            )[0]
-            phi_yy = torch.autograd.grad(
-                phi_y, Y, torch.ones_like(phi_y), create_graph=True, retain_graph=True
-            )[0]
-            residual = kappa * (phi_xx + phi_yy) - (ux * phi_x + uy * phi_y)
-            loss_pde = criterion(residual, torch.zeros_like(residual))
-            phi_pred_boundary = model(XY_boundary)
-            loss_bc = criterion(phi_pred_boundary, phi_boundary)
-            loss = loss_pde + loss_bc
-            losses.append(loss.item())
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            if (i + 1) % 100 == 0:
-                print(
-                    f"Epoch = {i+1}, loss = {loss.item()} = {loss_pde.item()} + {loss_bc.item()}"
-                )
-        torch.save(model.state_dict(), model_file)
+        model.train(lr=lr, epochs=epochs)
         t_end = time.time()
         duration_mins = (t_end - t_start) / 60
         title += f" , training time = {duration_mins} min"
-        title += f"\nloss = {loss.item()}, loss_pde = {loss_pde.item()}, loss_bc = {loss_bc.item()}"
+        torch.save(model.net.state_dict(), model_file)
     # ====Error evaluation=============================
-    max_err = 0
-    xy = XY.detach().numpy()
-    phi = model(XY)
-    phi_exact = phi_theory_np(xy[:, 0], xy[:, 1]).reshape(-1, 1)
-    phi_pred = phi.detach().numpy()
-    max_err = np.max(np.fabs(phi_exact - phi_pred))
-    title += f", max_abs_error = {max_err}"
-    print(f"max_err = {max_err}")
-    N_diag = 101
-    xy_antidiag = np.concatenate(
-        (
-            np.linspace(0, Lx, N_diag).reshape(-1, 1),
-            np.linspace(0, Ly, N_diag).reshape(-1, 1),
-        ),
-        axis=1,
-    )
-    XY_antidiag = torch.FloatTensor(xy_antidiag)
-    phi_antidiag_exact = phi_theory_np(xy_antidiag[:, 0], xy_antidiag[:, 1])
-    phi_antidiag_pred = model(XY_antidiag)
-    phi_antidiag_pred = phi_antidiag_pred.detach().numpy()
+    with torch.no_grad():
+        max_err = 0
+        xy = model.XY.detach().numpy()
+        phi = model.net(model.XY)
+        phi_exact = phi_theory_np(xy[:, 0], xy[:, 1]).reshape(-1, 1)
+        phi_pred = phi.detach().numpy()
+        max_err = np.max(np.fabs(phi_exact - phi_pred))
+        title += f", max_abs_error = {max_err}"
+        print(f"max_err = {max_err}")
+        N_diag = 101
+        xy_antidiag = np.concatenate(
+            (
+                np.linspace(0, Lx, N_diag).reshape(-1, 1),
+                np.linspace(0, Ly, N_diag).reshape(-1, 1),
+            ),
+            axis=1,
+        )
+        XY_antidiag = torch.FloatTensor(xy_antidiag)
+        phi_antidiag_exact = phi_theory_np(xy_antidiag[:, 0], xy_antidiag[:, 1])
+        phi_antidiag_pred = model.net(XY_antidiag)
+        phi_antidiag_pred = phi_antidiag_pred.detach().numpy()
 
-    xy_diag = np.concatenate(
-        (
-            np.linspace(0, Lx, N_diag).reshape(-1, 1),
-            np.linspace(Ly, 0, N_diag).reshape(-1, 1),
-        ),
-        axis=1,
-    )
-    XY_diag = torch.FloatTensor(xy_diag)
-    phi_diag_exact = phi_theory_np(xy_diag[:, 0], xy_diag[:, 1])
-    phi_diag_pred = model(XY_diag)
-    phi_diag_pred = phi_diag_pred.detach().numpy()
+        xy_diag = np.concatenate(
+            (
+                np.linspace(0, Lx, N_diag).reshape(-1, 1),
+                np.linspace(Ly, 0, N_diag).reshape(-1, 1),
+            ),
+            axis=1,
+        )
+        XY_diag = torch.FloatTensor(xy_diag)
+        phi_diag_exact = phi_theory_np(xy_diag[:, 0], xy_diag[:, 1])
+        phi_diag_pred = model.net(XY_diag)
+        phi_diag_pred = phi_diag_pred.detach().numpy()
     plt.figure(figsize=(12, 10))
     plt.subplot(3, 1, 1)
     plt.title(title)
